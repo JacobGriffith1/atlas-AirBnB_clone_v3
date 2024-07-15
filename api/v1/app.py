5086a86863d6df671f208d8d90ad5ffae06d605f
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-07-15 17:48:53 -0500

    nobody_home added to app.py, handles error 404
    Taking a break from that last one. Two of the checks are failing, but I haven't figured out how to fix them yet. Just gonna focus on getting as much done as I can rather than obsessing over one little thing.

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Script that runs Flask"""
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def teardown(exception):
     """Closes storage session"""
     storage.close
+
+
+@app.errorhandler(404)
+def nobody_home(error):
+    """Returns a JSON response for error 404"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":

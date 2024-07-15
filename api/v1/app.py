226d0f746f0d229aadac6818eb91a9288f58d8eb
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-07-15 01:24:17 -0500

    reworked part of app.py because I hated how I wrote it before. A lot nicer to look at this way, I think

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,9 +16,6 @@ def teardown(exception):
 
 
 if __name__ == "__main__":
-    HBNB_API_HOST = getenv(HBNB_API_HOST)
-    HBNB_API_PORT = getenv(HBNB_API_PORT)
-
-    host = '0.0.0.0' if not HBNB_API_HOST else HBNB_API_HOST
-    port = 5000 if not HBNB_API_PORT else HBNB_API_PORT
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(getenv("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
